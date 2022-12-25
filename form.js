2f4ed90e3974c55e0239b0673904fe6b6e39e4ba
thedayalsingh <thedayalsingh@gmail.com>
2022-12-25 18:30:22 +0530

    first form

---
+++ form.js
@@ -0,0 +1,27 @@
+// //console.dir(document)
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// //document.title=123
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// // console.log(document.all)
+// // console.log(document.all[10])
+// // document.all[10].textContent='Hello'
+// console.log(document.forms)
+// console.log(document.forms[0])
+// console.log(document.links)
+// console.log(document.images)
+
+// get elemnet by id
+// console.log(document.getElementById('header-title'))
+var headerTitle=document.getElementById('header-title')
+var header=document.getElementById('main-header')
+// console.log(headerTitle)
+// headerTitle.textContent='Hello'
+// headerTitle.innerText='Hello'
+// headerTitle.innerHTML=<h1>hhh</h1>
+// header.style.borderBottom='solid 3px black'
+
+//get element by class nmae
