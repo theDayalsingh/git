b8bc8e8931893eb69de55139b24f81bd156e7204
thedayalsingh <thedayalsingh@gmail.com>
2022-12-25 19:30:09 +0530

    tagname

--- form.js
+++ form.js
@@ -27,15 +27,28 @@ var header=document.getElementById('main-header')
 //get element by class nmae
 
 //get element by class nmae
-var items= document.getElementsByClassName('list-group-item')
-console.log(items)
-console.log(items[1])
-items[2].textContent="hello"
-items[2].style.fontWeight='bold'
-items[2].style.backgroundColor = "green"
+// var items= document.getElementsByClassName('list-group-item')
+// console.log(items)
+// console.log(items[1])
+// items[2].textContent="hello"
+// items[2].style.fontWeight='bold'
+// items[2].style.backgroundColor = "green"
 
-// sariyan class nu color krn li
+// // sariyan class nu color krn li
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold'
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold'
+// }
+
+//get element by tagname
+var li= document.getElementsByTagName('li')
+console.log(li)
+console.log(li[1])
+li[2].textContent="hello"
+li[2].style.fontWeight='bold'
+li[2].style.backgroundColor = "green"
+
+
+for(var i=0 ;i<li.length;i++){
+    li[i].style.fontWeight='bold'
 }
