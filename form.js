415a9af41ec0d6e2683cd3514bfeeea89ef0b41c
thedayalsingh <thedayalsingh@gmail.com>
2022-12-28 20:45:02 +0530

    filter

--- form.js
+++ form.js
@@ -1,17 +1,21 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // remove event from event lidt
 itemList.addEventListener('click',removeItem)
+// filter
+filter.addEventListener('keyup',filterItem)
+
 // Add item
 function addItem(e){
   e.preventDefault();
 
   // Get input value
   var newItem = document.getElementById('item').value;
-
+  var newItem1 = document.getElementById('item1').value;
   // Create new li element
   var li = document.createElement('li');
   // Add class
@@ -19,6 +23,7 @@ function addItem(e){
 //   console.log(li)
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newItem1));
 //create del button element
 var deleteBtn = document.createElement('button')
 //Add classes to del button
@@ -32,13 +37,6 @@ li.appendChild(deleteBtn)
 
   // Append li to list
   itemList.appendChild(li);
-  //create edit button element
-var editBtn = document.createElement('button')
-//Add classes to edit button
-editBtn.className='btn btn-primary btn-sm float-right '
-
-//Appemd text node
-editBtn.appendChild(document.createTextNode('Edit'))
 
 //Append button to li
 li.appendChild(editBtn)
@@ -54,3 +52,21 @@ if(e.target.classList.contains('delete')){
     }
 }
 } 
+
+//filter
+function filterItem(e){
+    // to lowercse
+    var text = e.target.value.toLowerCase()
+    // fet list tag
+    var items= itemList.getElementsByTagName('li')
+    // convert to array
+    Array.from(items).forEach(function(item){
+        var itemName=item.firstChild.textContent
+        var itemName1=item.childNodes[1].textContent
+        if( itemName.toLowerCase().indexOf(text)!=-1 || itemName1.toLowerCase().indexOf(text)!=-1){
+            item.style.display='block'
+        }else{
+            item.style.display='none'
+        }
+    })
+}
