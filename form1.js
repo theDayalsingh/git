d1dc1eec9908bcfe014920dffc9345d657e7dba5
thedayalsingh <thedayalsingh@gmail.com>
2022-12-30 01:54:57 +0530

    registrationform

--- form1.js
+++ form1.js
@@ -2,9 +2,15 @@ var form = document.getElementById('addForm');
 form.addEventListener('submit', addItem);
 function addItem(e){
   e.preventDefault();
-  var name = e.target.name.value
-  var age = e.target.age.value
-  var obj= {name,age}
+  const name = e.target.name.value
+  var email = e.target.email.value
+  var obj= {name,email}
   var st = JSON.stringify(obj)
-  localStorage.setItem("obj",st)
+  localStorage.setItem(email,st)
+  show(obj)
+}
+function show(user){
+    const pnode = document.getElementById("ulist")
+    const childli  = `<li>${user.name} - ${user.email}</li>`
+    pnode.innerHTML= pnode.innerHTML + childli
 }
